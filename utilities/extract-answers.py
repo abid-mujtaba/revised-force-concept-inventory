8de8caebd5639d3edc44501a3cce12606da3b61b
Abid H Mujtaba <abid.naqvi83@gmail.com>
2018-03-12 20:32:40 +0500

    Extract answers from tex file. Not stored yet.

--- utilities/extract-answers.py
+++ utilities/extract-answers.py
@@ -2,11 +2,29 @@
 A script for extracting answers from the specified test file (LaTeX) and storing it in the specified .csv file.
 """
 
-def main():
+import re
+import sys
 
-	pass
+re_ans = re.compile(r"\\answer{(\d+)}{(\w)}")
+
+
+def main(latex_file, csv_file):
+
+	with open(latex_file) as fin:
+
+		for lin in fin.readlines():
+
+			m = re_ans.match(lin.strip())
+
+			if m:
+				print("Answer to {0}: {1}".format(m.group(1), m.group(2)))
+
 
 
 if __name__ == '__main__':
 
-	main()
+	if len(sys.argv) != 3:
+		print("Error - Command should be used as: utilities.py <latex file> <output csv>")
+
+	else:
+		main(sys.argv[1], sys.argv[2])
