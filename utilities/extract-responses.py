8c30efc3b4ace48bbc9df439a0572f25ea5fb201
Abid H Mujtaba <abid.naqvi83@gmail.com>
2018-03-12 23:19:25 +0500

    Extract qid from scanned data

--- utilities/extract-responses.py
+++ utilities/extract-responses.py
@@ -2,12 +2,47 @@
 Extract the responses from the scanned in MCQ answer sheets.
 """
 
+import csv
 import sys
 
 
 def main(raw_file, out_file):
 
-	print("Hello, World")
+	with open(out_file, 'w', newline='') as fout:
+		
+		fieldnames = ['id', 'qid', 'rid']
+		count = 0
+
+		writer = csv.DictWriter(fout, fieldnames=fieldnames)
+		writer.writeheader()
+
+		with open(raw_file, newline='') as fin:
+
+			reader = csv.DictReader(fin)
+			# print(reader.fieldnames)
+
+			for row in reader:		# 'row' is an OrderedDict containing the data in the csv row
+
+				D = {}
+				count += 1
+
+				try:
+					# Process row to construct dictionary for the writer
+					D['id'] = count
+					D['qid'] = row['questionnaire_id']
+
+					writer.writerow(D)
+				
+
+				except ValidationError as e:
+					print("ValidationError: " + str(e))
+
+
+
+class ValidationError(Exception):
+
+	pass
+			
 
 
 if __name__ == '__main__':
