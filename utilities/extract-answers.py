9a168d64e1849072806ea6292d52557ea60c3c91
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2018-03-12 17:29:57 +0500

    Add data and empty answer extractor.

---
+++ utilities/extract-answers.py
@@ -0,0 +1,12 @@
+"""
+A script for extracting answers from the specified test file (LaTeX) and storing it in the specified .csv file.
+"""
+
+def main():
+
+	pass
+
+
+if __name__ == '__main__':
+
+	main()
